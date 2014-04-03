1e8b7f39dd8986c6ff98e7aedc8f98b25d8abe13
neoskys <jeremy.sigu@viacesi.fr>
2014-04-03 15:25:25 +0200

    objmobile + type

--- src/ObjetMobile.java
+++ src/ObjetMobile.java
@@ -1,4 +1,35 @@
 
 public class ObjetMobile {
+	
+	protected int vision;
+	protected int deplacement;
+	protected String typeObjet;
+	
+	public void setImage(String typeObjet)
+	{
+		
+	}
+	protected void setDeplacement(String typeObjet)
+	{
+		
+		
+	}
+	private void avancer()
+	{
+		
+	}
+	private void allerADroite()
+	{
+		
+	}
+	private void allerAGauche()
+	{
+		
+	}
+	private void demiTour()
+	{
+		
+	}
+	
 
 }
