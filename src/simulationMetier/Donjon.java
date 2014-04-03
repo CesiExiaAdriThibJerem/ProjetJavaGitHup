8677e26db19e979533810dce4376f32fe4a21d3a
Heavyshield <lasserre1992@gmail.com>
2014-04-03 17:12:44 +0200

    Attribut

--- src/simulationMetier/Donjon.java
+++ src/simulationMetier/Donjon.java
@@ -1,5 +1,23 @@
 package simulationMetier;
 
+import java.util.Random;
+
 public class Donjon {
+	
+	private int largeurGrille;
+	private int longueurGrille;
+	
+	private int nombreObstacle;
+	
+	private int nbrHumainClassique;
+	private int nbrHumainEclaireur;
+	private int nbrHumainTeleport;
+	private int nbrHumainBuffer;
+	
+	private Case cases[][];
+	private Random hasard;
+	
+	
+	
 
 }
