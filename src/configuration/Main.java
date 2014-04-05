0dc4eedc40bdaf7bec0db9cf155324dc0cc25ad7
AdrienMeltzer <adrien.meltzer@viacesi.fr>
2014-04-05 14:45:36 +0200

    Update FenetreConfig

--- src/configuration/Main.java
+++ src/configuration/Main.java
@@ -4,17 +4,17 @@ import simulationMetier.Donjon;
 
 public class Main {
 
+	@SuppressWarnings("unused")
 	public static void main(String[] args) {
-<<<<<<< HEAD
+
 		FenetreConfig config = new FenetreConfig();
 		
 		while (config.getValide() == false) {}
 			
 		Donjon monDonjon = new Donjon(config.getGrilleX(), config.getGrilleY(), config.getNbrObstacle(), config.getNbrHumainsClassique(), config.getNbrHumainsEclaireur(), config.getNbrHumainsTeleport(),config.getNbrHumainsBuffer());
-=======
-		Donjon monDonjon = new Donjon(30, 30, 10, 5, 5,5, 5);		
+	
 
->>>>>>> cf435a6f08023ecbc0d954fa8147a5b054008712
+
 	}
 
 }
