15314f0bc863d65dee19a321aaa3b6e6ab6346f2
AdrienMeltzer <adrien.meltzer@viacesi.fr>
2014-04-06 23:57:35 +0200

    Affichage Fonctionnel
    La partie affichage de grille et des elements mobiles est desormais
    fonctionnel

--- src/simulationInterface/Plateau.java
+++ src/simulationInterface/Plateau.java
@@ -2,7 +2,6 @@ package simulationInterface;
 
 import java.awt.GridLayout;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.ListIterator;
 
 import javax.swing.ImageIcon;
@@ -17,17 +16,20 @@ import simulationMetier.ElementsMobile;
 public class Plateau {
 	
 	private ArrayList<ElementsMobile> mobile;
+	private ListIterator<ElementsMobile> it;
 	private int largeur;
 	private int longueur;
 	private Case caseDonjons[][];
 	private JPanel contentpan;
+	private JFrame fen;
+	
 	
 	public Plateau(int largeur, int longueur, Case[][] casesDonjons) {
 		this.caseDonjons = new Case[largeur][longueur];
 		this.mobile = new ArrayList<>();
+		
 		this.largeur = largeur;
 		this.longueur = longueur;
-		int nb = 0;
 		
 		for (int y1 = 0; y1 < longueur; y1++) 
 		{
@@ -39,7 +41,7 @@ public class Plateau {
 		}
 			
 		
-		JFrame fen = new JFrame();
+		fen = new JFrame();
 		
 		fen.setTitle("Donjons");
 		fen.setLocationRelativeTo(null);
@@ -50,21 +52,13 @@ public class Plateau {
 		GridLayout cl = new GridLayout(largeur,longueur);
 		contentpan.setLayout(cl);
 		
-		ListIterator<ElementsMobile> it = mobile.listIterator();
+		
 		
 		for (int y = 0; y < largeur; y++) 
 		{
 			for (int x = 0; x < longueur; x++) 
 			{
-				while (it.hasNext() == true && nb == 0) {
-					ElementsMobile e = it.next();
-					
-					if (e.getX() == x && e.getY() == y) {
-						contentpan.add(new JLabel(new ImageIcon(e.getImage())));
-						nb=1;
-					}
-				}
-				nb=0;
+				
 				contentpan.add(new JLabel(new ImageIcon(this.caseDonjons[x][y].getImage())));							
 			}
 		}
@@ -82,22 +76,23 @@ public class Plateau {
 		contentpan = new JPanel();
 		contentpan.setLayout(new GridLayout(largeur, longueur));
 		int nb = 0;
-		ListIterator<ElementsMobile> it = mobile.listIterator();
+		
 		
 		for (int y = 0; y < largeur; y++) 
 		{
 			for (int x = 0; x < longueur; x++) 
 			{
-				
+				it = mobile.listIterator();
 				while (it.hasNext() == true) {
 					ElementsMobile e = it.next();
 					
-					if (e.getX() == x && e.getY() == y) {
-						System.out.println("HELLO !");
+					if ((e.getX() == y) &&( e.getY() == x)) {
+						
 						contentpan.add(new JLabel(new ImageIcon(e.getImage())));
 						nb = 1;
 					}
 				}
+				
 				if (nb == 0) {
 					contentpan.add(new JLabel(new ImageIcon(this.caseDonjons[x][y].getImage())));	
 				}
@@ -108,6 +103,8 @@ public class Plateau {
 									
 			}
 		}
+		fen.setContentPane(contentpan);
+		fen.setVisible(true);
 	}
 	
 }
