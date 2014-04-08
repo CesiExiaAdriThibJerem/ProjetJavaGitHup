c43683461c4218ac6382d511cd3450a31eb469d8
neoskys <jeremy.sigu@viacesi.fr>
2014-04-08 10:05:35 +0200

    algo humain

--- src/simulationMetier/HumainClassique.java
+++ src/simulationMetier/HumainClassique.java
@@ -1,5 +1,7 @@
 package simulationMetier;
 
+import java.util.Random;
+
 public class HumainClassique extends ElementsMobile {
 	
 	HumainClassique()
@@ -13,16 +15,55 @@ public class HumainClassique extends ElementsMobile {
 		this.y=getY();
 	}
 	
-	public void bouger() 
+	public void bougerClassique() 
 	{
-		if(vivant=true)
+		Random random = new Random();
+		if(vivant==true)
 		{
-			modifierDirection();
-			avancer(deplacement);	
+			direction= random.nextInt(4);
+			if (!this.donjon.getPosition(getX() + 1, getY()).estVide() && direction == sud ) {
+				
+				direction = random.nextInt(4); 				
+				
+			}
+			else if (this.donjon.getPosition(getX() + 1, getY()).estVide() && direction == sud )  
+			{
+				
+				setX(getX() + 1);
+				
+			}
+			else if (!this.donjon.getPosition(getX(), getY() + 1).estVide() && direction == est ) {
+				
+				random.nextInt(4);				
+				
+			}
+			else if (this.donjon.getPosition(getX(), getY() + 1).estVide() && direction == est ) {
+				setY(getY()+1);
+			}
+			
+			else if (!this.donjon.getPosition(getX() - 1, getY()).estVide() && direction == nord ) {
+				
+				random.nextInt(4);
+					
+				}
+			}
+			else if (this.donjon.getPosition(getX() - 1, getY()).estVide() && direction == nord ) {
+				setX(getX()-1);
+			}
+			
+			else if (!this.donjon.getPosition(getX(), getY()-1).estVide() && direction == ouest ) {
+				
+				random.nextInt(4);
+					
+							}
+			else if (this.donjon.getPosition(getX(), getY()-1).estVide() && direction == ouest  ) {
+				setY(getY()-1);
+			}
+		
 		}
 		
 	}
 
 	
 
-}
+
